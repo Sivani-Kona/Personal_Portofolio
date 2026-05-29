9e3697cb69b9ac9bce611c5a1256840ef6c29888
Sivani-Kona <rukminisivani201@gmail.com>
2026-05-29 17:00:59 +0530

    Add files via upload

---
+++ Project1.html/nav.js
@@ -0,0 +1,11 @@
+/*"use strict";
+const menu = document.querySelector(".ham");
+const navlist = document.querySelector(".navbar-list");
+if (menu && navlist) {
+  menu.addEventListener("click", () => {
+    navlist.classList.toggle("dropdown");
+  });
+}*/
+"use strict";
+const menu = document.querySelector(".topmenu");
+const toggle = document.querySelector(".menu-icon");
